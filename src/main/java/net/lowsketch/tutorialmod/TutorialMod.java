216906dd0d8ca7df9d14cf8739cd69960334cbde
LowSketch <lowsketchblue@gmail.com>
2025-01-05 18:13:00 -0600

    Idk man im just learning this.

---
+++ src/main/java/net/lowsketch/tutorialmod/TutorialMod.java
@@ -0,0 +1,19 @@
+package net.lowsketch.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+
+	// This logger is used to write text to the console and the log file.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric!");
+	}
+}
